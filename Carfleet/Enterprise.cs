d4f4d47597d019276863790034ffd50a425ea900
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2023-12-14 10:26:04 +0100

    feat: entreprise contructor

--- Carfleet/Enterprise.cs
+++ Carfleet/Enterprise.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 
 namespace Carfleet
@@ -6,16 +7,23 @@ namespace Carfleet
     public class Enterprise
     {
         #region private attributes
-        private string name;
-        private string street;
-        private string city;
-        private string phonenumber;
-        private string emailaddress;
+        private string _name;
+        private string _street;
+        private string _city;
+        private string _phonenumber;
+        private string _emailaddress;
+        private List<Vehicle> _vehicles = new List<Vehicle>();
+        private List<Driver> _drivers = new List<Driver>();
         #endregion private attributes
 
         #region public methods
         public Enterprise(string name, string street, string city, string phonenumber, string emailaddress)
         {
+            _name = name;
+            _street = street;
+            _city = city;
+            _phonenumber = phonenumber;
+            _emailaddress = emailaddress;
         }
 
         public void Add(Vehicle vehicleToAdd)
